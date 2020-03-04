60c194251fc136cec7c2c1c67703e1c885dfb90c
Marco Bertocco <marco@noe.fi.it>
2020-03-04 09:48:44 +0100

    Update touchid.js

--- www/touchid.js
+++ www/touchid.js
@@ -1,24 +1,24 @@
 var argscheck = require('cordova/argscheck'),
-	exec = require('cordova/exec');
+// var exec = require('cordova/exec');
 
 var touchid = {
 	isAvailable: function (successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
+		cordova.exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
 	},
 	save: function (key, password, message, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "save", [key, password, message]);
+		cordova.exec(successCallback, errorCallback, "TouchID", "save", [key, password, message]);
 	},
 	verify: function (key, message, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "verify", [key, message]);
+		cordova.exec(successCallback, errorCallback, "TouchID", "verify", [key, message]);
 	},
 	has: function (key, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "has", [key]);
+		cordova.exec(successCallback, errorCallback, "TouchID", "has", [key]);
 	},
 	delete: function (key, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "delete", [key]);
+		cordova.exec(successCallback, errorCallback, "TouchID", "delete", [key]);
 	},
 	setLocale: function (locale, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
+		cordova.exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
 	}
 };
 
